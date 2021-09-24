d80b105d0a97ff73f61d04d87d72c5edcb7e0f77
zd <1298698045@qq.com>
2021-09-24 20:51:16 +0800

    联调新闻、客户案例模板

--- 1/new_hospital/js/app.js
+++ 1/new_hospital/js/app.js
@@ -3,13 +3,35 @@ var mixin = {
         return {
             isArticle: false,
             hasRoute: this.hasOwnProperty('$route'),
-            selectMenu: ''
+            selectMenu: '',
+            isGotop:false,
+            qrCodeIdx: 0,
+            dialogVisible:false,
+            modalForm:{
+                unitName: '',
+                name: '',
+                phone: ''
+            },
+            rules:{
+                unitName:[
+                    { required: true, message: '请输入单位名称', trigger: 'blur' }
+                ],
+                name:[
+                    { required: true, message: '请输入姓名', trigger: 'blur' }
+                ],
+                phone:[
+                    { required: true, message: '请输入电话', trigger: 'blur' }
+                ]
+            }
         }
     },
     computed: {
         activeMenu: function() {
             return (this.selectMenu || this.typePath)
         }
+    },
+    mounted() {
+        window.addEventListener('scroll',this.handleIsScroll,true)
     },
     methods: {
         push: function(path) {
@@ -26,6 +48,30 @@ var mixin = {
         },
         onMenuMouseLeave: function() {
             this.selectMenu = ''
+        },
+        handleTabQrCode(idx){
+            this.qrCodeIdx = idx;
+        },
+        handleIsScroll() {
+            let scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+            scrolltop > 500 ? (this.isGotop = true) : (this.isGotop = false);
+        },
+        toTop() {
+            let top = document.documentElement.scrollTop || document.body.scrollTop;
+            // 实现滚动效果 
+            const timeTop = setInterval(() => {
+                document.body.scrollTop = document.documentElement.scrollTop = top -= 50;
+                if (top <= 0) {
+                    clearInterval(timeTop);
+                }
+            }, 10);
+        },
+        // 弹框
+        handleClose(){
+            this.dialogVisible = false;
+        },
+        handleOpen(){
+            this.dialogVisible = true;
         }
     }
 }
